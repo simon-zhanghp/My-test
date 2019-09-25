73807c23a8070faf7ab4bb3e2cab895d00b50e2c
364142997@qq.com <364142997@qq.com>
2019-09-25 10:40:53 +0800

    本地工程添加git

---
+++ hit-test-project/src/com/jiali/git/firstClass.java
@@ -0,0 +1,12 @@
+package com.jiali.git;
+
+public class firstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
